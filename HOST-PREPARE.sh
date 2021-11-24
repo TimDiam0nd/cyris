84594f2f614f26c76fe843b03b27fbf4c00ebeb0
TimDiam0nd <54599998+TimDiam0nd@users.noreply.github.com>
2021-11-24 11:53:28 +0000

    Update HOST-PREPARE.sh

--- HOST-PREPARE.sh
+++ HOST-PREPARE.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# This script is for preparing host Ubuntu Server 16.04 ready for CyRIS.
+# This script is for preparing host Ubuntu 18.04 ready for CyRIS.
+# do NOT run this as root, rather enter your password when prompted
 
+# install all the dependencies
 sudo apt update
 sudo apt install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils virt-manager python-pip python-paramiko tcpreplay sshpass pssh python-yaml python-scapy python-boto3 sendemail openssh-client openssh-server ifupdown wireshark
-# ssh-copy-id cyuser@localhost
-# needs to be run AFTER generating keys (ssh-keygen)
+
+ssh-keygen 
+ssh-copy-id cyuser@localhost

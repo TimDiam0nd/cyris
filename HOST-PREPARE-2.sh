38569da8a8d9eab59e7c0dd6d704073967038d10
TimDiam0nd <54599998+TimDiam0nd@users.noreply.github.com>
2021-11-24 11:35:32 +0000

    Update HOST-PREPARE-2.sh

--- HOST-PREPARE-2.sh
+++ HOST-PREPARE-2.sh
@@ -1,8 +1,11 @@
 # ensure you have done the following first:
-# ssh-keygen
 # ran the hostprepare script without any issues
 # allowed sudo without a password (run "sudo visudo" then append "cyuser ALL=(ALL) NOPASSWD: ALL")
+# downloaded the basevm files and unzipped them (default should be /home/cyuser/images/)
 
+ssh-copy-id cyuser@localhost
+
+# to solve qemu/libvirt permission denied errors
 sudo echo 'User = "root"' >> /etc/libvirt/qemu.conf
 sudo echo 'group = "root"' >> /etc/libvirt/qemu.conf
 sudo systemctl restart libvirtd
